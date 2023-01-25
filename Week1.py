d3976418cbba37e11688a31b2f2f8a214c427c47
Marcus Herstik <marcus@herstik.com>
2023-01-25 12:10:58 +1000

    Update Week1.py

--- Week1.py
+++ Week1.py
@@ -1,14 +1,18 @@
 
 a = "Marcus"  # firstname
-b = 'Herstik'
+b = '7'
 
 c = 4
-d = 5.0
+d = 5.1
 
-
+print(a+" "+ b)
+print(c * a)
 print(c,d)
-
+print(c+d)
 print(a,b)
 
 print(type(a), type(b), type(c), type(d))
-
+type(a)
+type(b)
+type(c)
+type(d)
