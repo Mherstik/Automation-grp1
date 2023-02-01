9d47e6ffe57c4659e49672984d70468e2c12c974
Marcus Herstik <marcus@herstik.com>
2023-02-01 13:06:34 +1000

    Create Wk2.py

---
+++ Wk2.py
@@ -0,0 +1,67 @@
+'''
+Description: Class- Automation Grp 1
+Date: Week 2 
+Author: Marcus Herstik
+
+Notes: Must be in class
+
+'''
+#print(2 + 2)
+# var1 = "Marcus"
+# var2 = "Herstik"
+#
+#type()
+# print(var1,var2)
+# print(var1 + var2)
+
+# firstName = input()
+# print(firstName)
+#print(input("What is your name: ")
+name = input("What is your name: ")
+
+## say hello name
+print("Hello", name)
+
+## ask for names age and store as age
+#age = int(input("How old are you: "))
+###
+#
+# # if the age type is int = OK
+# if type(age) is not str:
+#     print("This is not a string")
+# # else ask for age again
+# elif type(age) is str:
+#     print("This is a string")
+#     if type(int(age)) is int:
+#         print("This did work")
+#     else:
+#         print("They didn't put a number")
+# #####
+# else:
+#     print("Congratulations, you are 18")
+# 
+# 
+# print("Hello", name, "you are", age, "years old")
+
+## Tell them how old they will be next year
+#print("Next year you will be", age + 1)
+
+attempt = 0
+age = input("How old are you: ")
+while not age.isdigit():
+    print("attempt = ", attempt)
+    attempt = attempt +  1
+    print("attempt = ", attempt)
+    print("Please use a whole number!")
+    age = input("How old are you: ")
+    if attempt == 3:
+        print("Try again later")
+        age = 0
+        break
+    
+# convert age to integer
+age = int(age)
+
+
+
+
